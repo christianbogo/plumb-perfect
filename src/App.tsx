0aebda366feb2a9041548633a69d5212bad0f541
Christian Cutter <christianbcutter@yahoo.com>
2025-04-09 09:31:50 -0700

    feat: restructure project files and update application branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="libra-franklin">Welcome to Plumb Perfect!</h1>
+      <p>
+        This is a simple plumbing service application built with React and
+        TypeScript.
+      </p>
     </div>
   );
 }
